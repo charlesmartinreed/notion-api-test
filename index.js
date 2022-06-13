071f87b1d88934cff9a562c5d59162d2d196a345
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-06-12 23:41:36 -0500

    Create express route for /videos.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const getVideos = require("./services/notion");
+const express = require("express");
+const PORT = process.env.PORT || 5000;
 
-(async () => {
-  const myVideos = await getVideos();
-  console.log(myVideos);
-})();
+const app = express();
+
+app.use(express.static("public"));
+
+// ROUTES
+app.get("/videos", async (req, res) => {
+  const videos = await getVideos();
+  res.status(200).json(videos);
+});
+
+app.listen(PORT);
