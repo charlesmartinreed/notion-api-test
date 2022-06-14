939422b86521e5a2ee3bab8d80c823aef66c22bd
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-06-14 00:18:37 -0500

    Build skeletal layout in App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [notes, setNotes] = useState("");
+
+  function submitFormToNotion() {}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Employee Details</h1>
+        <h4>Please help keep our files up to date!</h4>
+
+        <div>
+          <p>Name</p>
+          <input
+            type="text"
+            id="name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <p>Phone Number</p>
+          <input
+            type="text"
+            id="phoneNumber"
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <p>Notes, Comments, etc.</p>
+          <textarea
+            id="notes"
+            cols={60}
+            rows={15}
+            onChange={(e) => setNotes(e.target.value)}
+          ></textarea>
+        </div>
+
+        <div>
+          <button onClick={submitFormToNotion}>Submit To Notion</button>
+        </div>
+      </div>
     </div>
   );
 }
